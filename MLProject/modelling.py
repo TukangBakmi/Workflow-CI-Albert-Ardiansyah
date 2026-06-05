dabbd81525efeb3b4dd43bf0520a0c4273191bf6
TukangBakmi <albertardiansyah06@gmail.com>
2026-06-05 22:45:34 +0700

    init

---
+++ MLProject/modelling.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+mlflow.sklearn.autolog()
+
+def main():
+
+    df = pd.read_csv("telco_churn_preprocessed.csv")
+
+    X = df.drop("Churn", axis=1)
+    y = df["Churn"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    with mlflow.start_run():
+
+        model = RandomForestClassifier(
+            n_estimators=100,
+            random_state=42
+        )
+
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        print(f"Accuracy : {accuracy:.4f}")
+        print(f"Precision: {precision:.4f}")
+        print(f"Recall   : {recall:.4f}")
+        print(f"F1 Score : {f1:.4f}")
+
+if __name__ == "__main__":
+    main()
